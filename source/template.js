2cd560a80aaa8decb5dd1e26a4ddd70991933640
Alex Bright <abrighttt@gmail.com>
2021-07-02 05:26:44 -0400

    template modified

--- source/template.js
+++ source/template.js
@@ -1,118 +1,70 @@
-const generateTeam = team => {
-
-    // <!-- manager -->
-
-const generateManager = manager => {
-        return `
-<section class="card employee-card">
-    <div class="card-header bg-primary text-white">
-        <h2 class="card-title">${manager.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${manager.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
-            <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
-        </ul>
-    </div>
-</section>
-        `;
-    };
-
-    // <!-- engineer -->
-
-const generateEngineer = engineer => {
-        return `
-<section class="card employee-card">
-    <div class="card-header bg-primary text-white">
-        <h2 class="card-title">${engineer.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${engineer.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
-            <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer">${engineer.getGithub()}</a></li>
-        </ul>
-    </div>
-</section>
-        `;
-    };
-
-    // <!-- intern -->
-    
-const generateIntern = intern => {
-        return `
-<section class="card employee-card">
-    <div class="card-header bg-primary text-white">
-        <h2 class="card-title">${intern.getName()}</h2>
-        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
-    </div>
-    <div class="card-body">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${intern.getId()}</li>
-            <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
-            <li class="list-group-item">School: ${intern.getSchool()}</li>
-        </ul>
-    </div>
-</section
-        `;
-    };
-
-    const html = [];
-
-    html.push(team
-        .filter(employee => employee.getRole() === "Manager")
-        .map(manager => generateManager(manager))
-    );
-    html.push(team
-        .filter(employee => employee.getRole() === "Engineer")
-        .map(engineer => generateEngineer(engineer))
-        .join("")
-    );
-    html.push(team
-        .filter(employee => employee.getRole() === "Intern")
-        .map(intern => generateIntern(intern))
-        .join("")
-    );
-
-    return html.join("");
-
+// manager
+const managerCard = (Manager) => {
+    const {name, id, email, number, role} = Manager
+    return `<section class = "column">
+        <div class = "card">
+            <div class = "cardContent">
+                <div class="cardHeader">
+                    <div class = "cardHeaderContent text-center bg-gradient bg-dark text-white">
+                        <h4 class = "cardName">${name}</h4>
+                        <p class = "cardRole">${role}</p>
+                    </div>
+                </div>
+                <div class="content text-center">
+                    <div class = "column"> ID: ${id} </div>
+                    <div class = "column"> Email: <a href="mailto:${email}">${email}</a></div>
+                    <div class = "column"> Office Number: ${number} </div>
+                </div>
+            </div>
+        </div>
+    </section>`  
 }
 
+// intern
 
-module.exports = team => {
-
-    return `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>My Team</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/c502137733.js"></script>
-</head>
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading bg-danger">
-                <h1 class="text-center text-white">My Team</h1>
+const internCard = (Intern) => {
+    const {name, id, email, school, role} = Intern
+    return `<section class = "column">
+        <div class = "card">
+            <div class = "cardContent">
+                <div class = "cardHeader">
+                    <div class = "cardHeaderContent text-center bg-gradient bg-secondary text-white">
+                        <h4 class = "cardName">${name}</h4>
+                        <p class = "cardRole">${role}</p>
+                    </div>
+                </div>
+                <div class="content text-center">
+                    <div class = "column"> ID: ${id} </div>
+                    <div class = "column"> Email: <a href="mailto:${email}">${email}</a></div>
+                    <div class = "column"> School: ${school}</div>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="row team-area col-12 d-flex justify-content-center">
-                ${generateTeam(team)}
+    </section>` 
+}
+
+// engineer
+const engineerCard = (Engineer) => {
+    const {name, id, email, gitHub, role} = Engineer
+    return `<section class = "column">
+        <div class = "card">
+            <div class = "cardContent">
+                <div class = "cardHeader">
+                    <div class = "cardHeaderContent text-center bg-gradient bg-secondary text-white">
+                        <h4 class = "cardName">${name}</h4>
+                        <p class = "cardRole">${role}</p>
+                    </div>
+                </div>
+                <div class="content text-center">
+                    <div class = "column"> ID: ${id} </div>
+                    <div class = "column"> Email: <a href="mailto:${email}">${email}</a></div>
+                    <div class = "column"> GitHub Userame: <a href="https://github.com/${gitHub}" target="_blank">${gitHub}</a></div>
+                </div>
             </div>
         </div>
-    </div>
-</body>
-</html>
-    `;
-};
+    </section>`  
+}
+
+exports.managerCard=managerCard
+exports.engineerCard=engineerCard
+exports.internCard=internCard
